6752e67c92b00d19da5b87b223cef65d64495b40
icecolla <icecoladrink@gmail.com>
2020-11-16 17:44:42 +0300

    Add First Project

---
+++ Project_1_ecs_2d/Assets/ECSManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+public class ECSManager : MonoBehaviour
+{
+    EntityManager manager;
+
+    public GameObject objectPrefab;
+    public int numObject = 1000;
+
+    private void Start()
+    {
+        manager = World.DefaultGameObjectInjectionWorld.EntityManager;
+        var settings = GameObjectConversionSettings.FromWorld(World.DefaultGameObjectInjectionWorld, null);
+        var prefab = GameObjectConversionUtility.ConvertGameObjectHierarchy(objectPrefab, settings);
+
+        for (int i = 0; i < numObject; i++)
+        {
+            var instance = manager.Instantiate(prefab);
+            var position = transform.TransformPoint(new float3(UnityEngine.Random.Range(-50, 50), UnityEngine.Random.Range(0, 100), UnityEngine.Random.Range(-50, 50)));
+            manager.SetComponentData(instance, new Translation { Value = position });
+            manager.SetComponentData(instance, new Rotation { Value = new quaternion(0, 0, 0, 0) });
+        }
+    }
+}
---
+++ Project_1_ecs_2d/Assets/MoveSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+using Unity.Transforms;
+using Unity.Jobs;
+
+public class MoveSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var jobHandle = Entities.WithName("MoveSystem").
+                        ForEach((ref Translation position, ref Rotation rotation) =>
+                        {
+                            //position.Value += 0.1f * math.forward(rotation.Value);
+                            position.Value.y += 0.1f;
+
+                            if (position.Value.y > 100)
+                                position.Value.y = 0;
+                        })
+                        .Schedule(inputDeps);
+        return jobHandle;
+    }
+}
